f14d0040ac9fafb7a80c7f48d28554b7731485d7
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-07-31 23:30:00 -0300

    feat: add resource not found error to error handler

--- src/utils/error-handler.ts
+++ src/utils/error-handler.ts
@@ -3,6 +3,7 @@ import { ZodError } from "zod";
 import { env } from "../env";
 import { InvalidTripStartDate } from "../errors/invalid-trip-start-date";
 import { InvalidTripEndDate } from "../errors/invalid-trip-end-date";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error";
 
 type FastifyErrorHandler = FastifyInstance["errorHandler"];
 
@@ -23,6 +24,10 @@ export const errorHandler: FastifyErrorHandler = (error, _, reply) => {
     error instanceof InvalidTripEndDate
   ) {
     return reply.status(400).send({ message: error.message });
+  }
+
+  if (error instanceof ResourceNotFoundError) {
+    return reply.status(404).send({ message: "Not found." });
   }
 
   return reply.status(500).send({ message: "Internal server error." });

4f7cb0b75786113aed482d6a59c49288d208b7eb
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-08-01 21:49:49 -0300

    feat: create prisma activities repository

---
+++ src/repositories/prisma/prisma-activities-repository.ts
@@ -0,0 +1,21 @@
+import { Prisma } from "@prisma/client";
+import { prisma } from "../../lib/prisma";
+import { ActivitiesRepository } from "../activities-repository";
+
+export class PrismaActivitiesRepository implements ActivitiesRepository {
+  async findById(id: string) {
+    const activity = await prisma.activity.findUnique({
+      where: { id },
+    });
+
+    return activity;
+  }
+
+  async create(data: Prisma.ActivityCreateInput) {
+    const activity = await prisma.activity.create({
+      data,
+    });
+
+    return activity;
+  }
+}

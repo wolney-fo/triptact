e87c4cb609b1bf22dcefafc9efecfdcee1e45957
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-08-02 23:02:54 -0300

    feat: create get activities route

---
+++ src/http/activities/get-by-trip.ts
@@ -0,0 +1,43 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { makeGetActivitiesUseCase } from "../../use-cases/factories/make-get-activities";
+
+export async function getByTrip(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/trips/:tripId/activities",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string(),
+        }),
+        response: {
+          200: z.object({
+            activities: z.array(
+              z.object({
+                date: z.date(),
+                activities: z.array(
+                  z.object({
+                    id: z.string(),
+                    title: z.string(),
+                    occurs_at: z.date(),
+                    trip_id: z.string(),
+                  })
+                ),
+              })
+            ),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { tripId } = request.params;
+
+      const getActivitiesUseCase = makeGetActivitiesUseCase();
+
+      const { activities } = await getActivitiesUseCase.execute({ tripId });
+
+      return reply.status(200).send({ activities });
+    }
+  );
+}
--- src/http/activities/routes.ts
+++ src/http/activities/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
+import { getByTrip } from "./get-by-trip";
 
 export async function activitiesRoutes(app: FastifyInstance) {
   app.register(create);
+  app.register(getByTrip);
 }

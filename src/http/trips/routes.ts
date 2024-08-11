605436f4495969db4a21379d32d3ce3fe2517f70
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-08-10 22:48:50 -0300

    feat: create update trip route

--- src/http/trips/routes.ts
+++ src/http/trips/routes.ts
@@ -1,8 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { create } from "./create";
 import { confirm } from "./confirm";
+import { update } from "./update";
 
 export async function tripsRoutes(app: FastifyInstance) {
   app.register(create);
   app.register(confirm);
+  app.register(update);
 }
---
+++ src/http/trips/update.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { makeUpdateTripUseCase } from "../../use-cases/factories/make-update-trip";
+
+export async function update(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().put(
+    "/trips/:tripId",
+    {
+      schema: {
+        params: z.object({
+          tripId: z.string().uuid(),
+        }),
+        body: z.object({
+          destination: z.string(),
+          starts_at: z.coerce.date(),
+          ends_at: z.coerce.date(),
+        }),
+        response: {
+          200: z.object({
+            tripId: z.string().uuid(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { tripId } = request.params;
+      const { destination, starts_at, ends_at } = request.body;
+
+      const updateTripUseCase = makeUpdateTripUseCase();
+
+      const { trip } = await updateTripUseCase.execute({
+        tripId,
+        destination,
+        starts_at,
+        ends_at,
+      });
+
+      return reply.status(200).send({ tripId: trip.id });
+    }
+  );
+}

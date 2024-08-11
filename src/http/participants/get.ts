260728a8e311b67f1a4c72bea31b24b44d126777
Wolney Oliveira <wolney.f.oliveira@outlook.com>
2024-08-10 23:32:59 -0300

    feat: create get participant route

---
+++ src/http/participants/get.ts
@@ -0,0 +1,38 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { makeGetParticipantUseCase } from "../../use-cases/factories/make-get-participant";
+
+export async function get(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/participants/:participantId",
+    {
+      schema: {
+        params: z.object({
+          participantId: z.string().uuid(),
+        }),
+        response: {
+          200: z.object({
+            participant: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string().email(),
+              is_confirmed: z.boolean(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { participantId } = request.params;
+
+      const getParticipantUseCase = makeGetParticipantUseCase();
+
+      const { participant } = await getParticipantUseCase.execute({
+        participantId,
+      });
+
+      return reply.status(200).send({ participant });
+    }
+  );
+}
--- src/http/participants/routes.ts
+++ src/http/participants/routes.ts
@@ -1,10 +1,12 @@
 import { FastifyInstance } from "fastify";
 import { confirm } from "./confirm";
 import { createInvite } from "./create-invite";
+import { get } from "./get";
 import { getByTrip } from "./get-by-trip";
 
 export async function participantsRoutes(app: FastifyInstance) {
   app.register(confirm);
   app.register(createInvite);
+  app.register(get);
   app.register(getByTrip);
 }
